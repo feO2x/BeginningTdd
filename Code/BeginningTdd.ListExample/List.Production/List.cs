e6d794f0d6e627e1c63b5c9fa5120d102ebcac35
Kenny Pflug <kenny.pflug@live.de>
2015-02-17 14:07:57 +0100

    List reflects count when items are added

---
+++ Code/BeginningTdd.ListExample/List.Production/List.cs
@@ -0,0 +1,15 @@
+﻿
+namespace List.Production
+{
+	public class List<T>
+	{
+		private int _count;
+
+		public int Count { get { return _count; } }
+
+		public void Add(int v)
+		{
+			_count++;
+		}
+	}
+}
---
+++ Code/BeginningTdd.ListExample/List.Tests/ListTests.cs
@@ -0,0 +1,30 @@
+﻿using List.Production;
+using Xunit;
+using TestData = System.Collections.Generic.IEnumerable<object[]>;
+
+namespace List.Tests
+{
+    public class ListTests
+    {
+		[Theory]
+		[MemberData("CountTestDataForAdd")]
+        public void CountReflectsTheNumberOfItemsThatWereAddedToTheList(int[] itemsToAdd)
+		{
+			var testTarget = new List<int>();
+
+			foreach (var item in itemsToAdd)
+			{
+				testTarget.Add(item);
+			}
+
+			Assert.Equal(itemsToAdd.Length, testTarget.Count);
+		}
+
+		public static readonly TestData CountTestDataForAdd = new[]
+														      {
+														        	new object[] { new int[] { 1, 2, 3, 4 } },
+														      		new object[] { new int[] { 33, 22, 11 } },
+														      		new object[] { new int[] { 76, 103, 105, 87, 66, 53, 89 } }
+														      };
+    }
+}
